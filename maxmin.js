ec84bfda778455457b41e2c0d7241c6c3c60e593
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2022-08-05 14:51:58 +0530

    js maxmin function

---
+++ maxmin.js
@@ -0,0 +1,11 @@
+function sumMaxMin(a, b, c, d, e) { 
+
+    const arr = [a, b, c, d, e];
+
+    return Math.max(...arr)+Math.min(...arr);
+ }
+
+ console. log(sumMaxMin(100, 100, -200, 300, 0));
+
+ console. log(sumMaxMin(1, 3, 2, 4, 5));
+console. log(sumMaxMin(-1000, -2000, -10, -120, -60)); 
